0890aa26920196825359a70f6d7b297f94bfe53a
Jean Pierre Ortiz <ortizmurciajeanpierre@gmail.com>
2024-12-24 23:02:50 +0000

    feat: add global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from '@/App.tsx';
 import { setup } from 'goober';
-import { prefix } from 'goober/prefixer';
+import { createGlobalStyles } from 'goober/global';
 
-setup(React.createElement, prefix);
+setup(React.createElement);
+
+const GlobalStyles = createGlobalStyles`
+  html,
+  body {
+    margin: 0;
+    overflow: hidden;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  .split {
+    display: flex;
+    flex-direction: row;
+}
+
+  .gutter {
+      background-color:rgb(40, 194, 255);
+      background-repeat: no-repeat;
+      background-position: 50%;
+  }
+
+  .gutter.gutter-horizontal {
+      background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAeCAYAAADkftS9AAAAIklEQVQoU2M4c+bMfxAGAgYYmwGrIIiDjrELjpo5aiZeMwF+yNnOs5KSvgAAAABJRU5ErkJggg==');
+      cursor: col-resize;
+  }
+`;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
