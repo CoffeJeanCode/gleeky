5de8458459bcfb708f1f907648aaac9974d88267
Jean Pierre Ortiz <ortizmurciajeanpierre@gmail.com>
2025-04-16 23:55:08 -0500

    feat: add data types

--- src/models/code.ts
+++ src/models/code.ts
@@ -1,6 +1,23 @@
 export type Code = string;
-
-export type DataType = 'string' | 'number' | 'boolean' | 'object' | 'array' | 'undefined' | 'function' | 'symbol' | 'bigint' | "null" | "error";
+export type DataType =
+  | 'undefined'
+  | 'null'
+  | 'boolean'
+  | 'number'
+  | 'bigint'
+  | 'string'
+  | 'symbol'
+  | 'function'
+  | 'array'
+  | 'object'
+  | 'map'
+  | 'set'
+  | 'date'
+  | 'regexp'
+  | 'error'
+  | 'class'
+  | 'circular'
+  | 'unknown';
 export type OutputType = "error" | "log" | "warn" | "table";
 
 export interface Data { type: DataType, value: string }
